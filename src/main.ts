58368323b0fcba531d271d9c3d19b80c68dbe273
LeonelCeballos <leonelalejandroceballos@gmail.com>
2025-02-06 12:45:53 -0600

    chore(db): database configuration and connection
    adds database configuration with TypeORM
    adds configuration for environment variables with dotenv dependency

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppDataSource } from './data-source';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  AppDataSource.initialize()
+    .then(() => console.log('Database connected'))
+    .catch((err) => console.error('Database connection error:', err));
 }
 bootstrap();
