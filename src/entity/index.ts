d67d7a5e6e62b9313ad2281b5a6217562f92c85f
cesarj45 <cesarmunozrj45@gmail.com>
2025-02-19 10:50:05 -0600

    chore: delete the old version of the entity files.

--- src/entity/index.ts
+++
@@ -1,5 +0,0 @@
-export { Appointment } from './Appointment';
-export { Customer } from './Customer';
-export { Service } from './Service';
-export { User } from './User';
-export { Role } from './Role';
