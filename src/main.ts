3cea16f56ef0a2632c5ee2b77f3f30026bb774c4
LeonelCeballos <leonelalejandroceballos@gmail.com>
2025-02-27 16:42:44 -0600

    chore: change domain in cors configuration

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://tudominio.com'],
+    origin: [
+      'http://localhost:3000',
+      'https://mindticas2025-front-jxlcr32so-michelada-interns.vercel.app/',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
