9412a82dc203ae89e5e19a2a7e4650c33f46189d
cesarj45 <cesarmunozrj45@gmail.com>
2025-03-04 16:58:24 -0600

    add seed.service to index

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,4 @@
 export { default as TreatmentService } from './treatment.service';
 export { default as AppointmentService } from './appointment.service';
 export { default as CustomerService } from './customer.service';
+export { default as SeedService } from './seed.service';
