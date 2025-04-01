1d6f967f044fae5e5ccfcdbb6433a250e21f37cd
LeonelCeballos <leonelalejandroceballos@gmail.com>
2025-04-01 12:46:55 -0600

    feature: Security Tweaks

--- src/controllers/treatment.controller.ts
+++ src/controllers/treatment.controller.ts
@@ -6,9 +6,11 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { TreatmentService } from '../services';
 import { CreateTreatmentDTO, UpdateTreatmentDTO } from '../dtos';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('treatment')
 export default class TreatmentController {
@@ -18,22 +20,29 @@ export default class TreatmentController {
   getAllTreatments() {
     return this.treatmentService.getAllTreatments();
   }
+
   @Get(':id')
   getTreatment(@Param('id') id: number) {
     return this.treatmentService.getTreatment(id);
   }
+
   @Post()
+  @UseGuards(AuthGuard)
   createTreatment(@Body() dto: CreateTreatmentDTO) {
     return this.treatmentService.createTreatment(dto);
   }
+
   @Patch(':id')
+  @UseGuards(AuthGuard)
   async updateTreatment(
     @Param('id') id: number,
     @Body() updateTreatmentDTO: UpdateTreatmentDTO,
   ) {
     return await this.treatmentService.updateTreatment(id, updateTreatmentDTO);
   }
+
   @Delete(':id')
+  @UseGuards(AuthGuard)
   async deleteTreatment(@Param('id') id: number) {
     return this.treatmentService.deleteTreatment(id);
   }
