90173e078006a1e9df1cdb3be5eb85816d8d413c
D. S. <1130945+dsalazar357@users.noreply.github.com>
2025-04-11 13:01:07 -0600

    fix: adds express handler to use with serverless aws management

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,23 @@ import { AppModule } from './app.module';
 import { AppDataSource } from './data-source';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import { createServer, proxy } from 'aws-serverless-express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+const expressApp = express();
+
+async function createNestApp(expressInstance: express.Express) {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressInstance));
   const logger = new Logger('Bootstrap');
   const configService = app.get(ConfigService);
+
   app.enableCors({
     origin: ['http://localhost:3000', configService.get('auth.urlWeb')],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
   });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -20,12 +27,22 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  AppDataSource.initialize()
+
+  await app.init();
+  await AppDataSource.initialize()
     .then(() => logger.log('Database connected'))
     .catch((err) =>
       logger.error(`Database connection error: ${err.message}`, err.stack),
     );
-  await app.listen(3000);
+  return expressInstance;
 }
 
-bootstrap();
+let server: any;
+
+export default async function handler(req: any, res: any) {
+  if (!server) {
+    const expressInstance = await createNestApp(expressApp);
+    server = createServer(expressInstance);
+  }
+  return proxy(server, req, res);
+}
